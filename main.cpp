92cf4d7c5004d0c1ec51e88f4f6b77f37172de52
Антон Шмидт <schmidt.ajax@gmail.com>
2022-02-18 12:10:04 +0700

    Solution

--- main.cpp
+++ main.cpp
@@ -1,11 +1,28 @@
 #include <cassert>
+#include <cmath>
 #include <iostream>
+#include <vector>
+
+using namespace std;
 
 class Solution {
  public:
   bool checkPerfectNumber(int num) {
-    // FIXME
-    return false;
+    if (num < 6) return false;
+
+    int sum = 1;
+    int limit = static_cast<int>(sqrt(num)) + 1;
+    for (int i = 2; i < limit; ++i) {
+      if (num % i == 0) {
+        sum += i;
+        const int k = num / i;
+        if (k != i) {
+          sum += k;
+        }
+      }
+    }
+
+    return sum == num;
   }
 };
 
@@ -14,6 +31,7 @@ void TestCheckPerfectNumber() {
   assert(s.checkPerfectNumber(6));
   assert(not s.checkPerfectNumber(7));
   assert(s.checkPerfectNumber(28));
+  assert(not s.checkPerfectNumber(120));
 //  assert(s.checkPerfectNumber());
 }
 
